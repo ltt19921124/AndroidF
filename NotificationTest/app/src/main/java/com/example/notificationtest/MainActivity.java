aa5db617fae2a6272f73bdc5d0635b2ab312c34e
ltt19921124 <ltt19921124@163.com>
2017-06-17 15:34:07 +0800

    k
    k

--- NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.notificationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +13,20 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button sendNotice = (Button) findViewById(R.id.send_notice);
         sendNotice.setOnClickListener(this);
-
     }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.send_notice:
+                Intent intent = new Intent(this,NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
                 NotificationManager manager = (NotificationManager) getSystemService
                         (NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(this)
@@ -32,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
+                        .setAutoCancel(true)
                         .build();
                 manager.notify(1,notification);
                 break;
