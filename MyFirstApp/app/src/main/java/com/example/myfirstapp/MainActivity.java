3bf41766000bd73b09ff2c8d26b7f6c7d2135b44
ltt19921124 <ltt19921124@163.com>
2017-06-17 13:03:59 +0800

    k
    k

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.news_articles);
     }
     public void sendMessage(View view) {
         Intent intent = new Intent(this,DisplayMessageActivity.class);
