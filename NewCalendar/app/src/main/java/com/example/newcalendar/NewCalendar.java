db69ae38a27ee658224e4d368ef7344fc79e1366
ltt19921124 <ltt19921124@163.com>
2017-06-22 21:22:52 +0800

    k
    k

---
+++ NewCalendar/app/src/main/java/com/example/newcalendar/NewCalendar.java
@@ -0,0 +1,34 @@
+package com.example.newcalendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Tian Lu on 2017/6/22.
+ */
+
+public class NewCalendar extends LinearLayout {
+
+    public NewCalendar(Context context) {
+        super(context);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
+
+
+
+
+
+
+
